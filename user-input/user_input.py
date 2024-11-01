b0832478059d39c35c11f9085e570479d3c10044
Elly Otieno <okothelli@gmail.com>
2024-11-01 12:57:15 +0300

    ask user for inputs

---
+++ user-input/user_input.py
@@ -0,0 +1,7 @@
+name = input('Enter your name: ')
+
+age = int(input('Enter your age: '))
+
+location = input('Where do you live: ')
+
+print(f'Hello {name}, you`re {age} years old and you live in {location}')
